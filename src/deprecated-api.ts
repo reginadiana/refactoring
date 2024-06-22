8ac3620c87b7ec77e197fea77e2ca967881dadcf
Diana Regina <dianaregina22@outlook.com.br>
2024-06-22 08:00:42 -0300

    tests: add cenarios, init refactor

---
+++ src/deprecated-api.ts
@@ -0,0 +1,88 @@
+/**
+ * Código original sem refatoração
+ */
+
+import crypto from "crypto";
+import express, { Express, Request, Response } from "express";
+import pgp from "pg-promise";
+import { validate } from "./validateCpf";
+
+const app: Express = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+// const pgport = 5432;
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Express + TypeScript Server");
+// 	console.log("[server] Server get /")
+// });
+
+app.post("/signup", async function (req, res) {
+	let result;
+	// const connection = pgp()(`postgres://postgres:123456@localhost:${pgport}/app`);
+	try {
+		const id = crypto.randomUUID();
+
+		const [acc] = '' // await connection.query("select * from cccat15.account where email = $1", [req.body.email]);
+		if (!acc) {
+
+			if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
+				if (req.body.email.match(/^(.+)@(.+)$/)) {
+
+					if (validate(req.body.cpf)) {
+						if (req.body.isDriver) {
+							if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
+								// await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
+								
+								const obj = {
+									accountId: id
+								};
+								result = obj;
+							} else {
+								// invalid car plate
+								result = -5;
+							}
+						} else {
+							// await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
+
+							const obj = {
+								accountId: id
+							};
+							result = obj;
+						}
+					} else {
+						// invalid cpf
+						result = -1;
+					}
+				} else {
+					// invalid email
+					result = -2;
+				}
+
+			} else {
+				// invalid name
+				result = -3;
+			}
+
+		} else {
+			// already exists
+			result = -4;
+		}
+		if (typeof result === "number") {
+			res.status(422).send(result + "");
+		} else {
+			res.json(result);
+		}
+	} finally {
+		// await connection.$pool.end();
+	}
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+// app.listen(`postgres://postgres:123456@localhost:${pgport}/app`, () => {
+//   console.log(`[postgress]: Pg is running at http://localhost:${pgport}`);
+// });
--- test/api.test.ts
+++ test/api.test.ts
@@ -4,15 +4,97 @@ axios.defaults.validateStatus = function () {
 	return true;
 }
 
-test("Deve criar uma conta para o passageiro", async function () {
-	const input = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "87748248800",
-		isPassenger: true
+const params = {
+	name: 'Diana Rodriguez',
+	email:`diana.rodriguez${Math.random()}@gmail.com`,
+	cpf: "87748248800",
+	isPassenger: true
+}
+
+test("should create passenger account successfully", async function () {	
+	const result = await axios.post("http://localhost:3000/signup", params);
+
+	expect(result.status).toBe(200);
+	expect(result.data.accountId).toBeDefined();
+});
+
+test("should create driver account successfully", async function () {	
+	const payload = {
+		...params,
+		isPassenger: false,
+		isDriver: true,
+		carPlate: 'ABC1234'
+	}
+
+	const result = await axios.post("http://localhost:3000/signup", payload);
+
+	expect(result.status).toBe(200);
+	expect(result.data.accountId).toBeDefined();
+});
+
+it('CPF should be valid', async () => {
+	const payload = {
+		...params,
+		cpf: '1111111',
 	};
 	
-	const output = await axios.post("http://localhost:3000/signup", input);
+	const result = await axios.post("http://localhost:3000/signup", payload);
+
+	expect(result.status).toBe(422);
+	expect(result.data).toBe(-1);
+	expect(result.data.accountId).not.toBeDefined();
+})
+
+it('email should be valid', async () => {
+	const payload = {
+		...params,
+		email: 'invalid-email',
+	};
 	
-	console.log(output.status, output.data);
+	const result = await axios.post("http://localhost:3000/signup", payload);
+
+	expect(result.status).toBe(422);
+	expect(result.data).toBe(-2);
+	expect(result.data.accountId).not.toBeDefined();
+})
+
+it('name should not be empty', async () => {
+	const payload = {
+		...params,
+		name: '',
+	};
+
+	const result = await axios.post("http://localhost:3000/signup", payload);
+
+	expect(result.status).toBe(422);
+	expect(result.data).toBe(-3);
+	expect(result.data.accountId).not.toBeDefined();
+});
+
+it('name should be valid', async () => {
+	const payload = {
+		...params,
+		name: 'Diana',
+	};
+
+	const result = await axios.post("http://localhost:3000/signup", payload);
+
+	expect(result.status).toBe(422);
+	expect(result.data).toBe(-3);
+	expect(result.data.accountId).not.toBeDefined();
+});
+
+test("car plate should be valid", async function () {	
+	const payload = {
+		...params,
+		isPassenger: false,
+		isDriver: true,
+		carPlate: '1111'
+	}
+
+	const result = await axios.post("http://localhost:3000/signup", payload);
+
+	expect(result.status).toBe(422);
+	expect(result.data).toBe(-5);
+	expect(result.data.accountId).not.toBeDefined();
 });
